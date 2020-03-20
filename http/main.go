58fa8f6abe7bbb4af9038831b7c921c2d25d0d6d
devplayg <devplayg@gmail.com>
2020-03-20 18:35:30 +0900

    added examples of http and https

--- http/main.go
+++ http/main.go
@@ -2,34 +2,21 @@ package main
 
 import (
 	"flag"
-	"html/template"
-	"io/ioutil"
+	"github.com/devplayg/http_http2"
 	"log"
 	"net/http"
-	"time"
 )
 
 var (
 	addr = flag.String("addr", ":8000", "Listen address")
 )
 
-//func Response(w http.ResponseWriter) {
-//	vars := map[string]interface{} {
-//		"ts":  time.Now().UnixNano(),
-//		"arr": make([]int, 60),
-//	}
-//
-//	html, _ := ioutil.ReadFile("../index.html")
-//	t := template.Must(template.New("html-tmpl").Parse(string(html)))
-//	t.Execute(w, vars)
-//}
-
 func main() {
 	flag.Parse()
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		Response(w)
+		http_http2.Response(w)
 	})
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
