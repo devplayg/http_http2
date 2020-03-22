0c95d589a5b2fc571a628031e37ff786752d49ee
devplayg <devplayg@gmail.com>
2020-03-22 10:01:29 +0900

    added https fixedi http2 changed html changed server push

--- http2/main.go
+++ http2/main.go
@@ -1,41 +1,63 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"github.com/devplayg/http_http2"
 	"log"
 	"net/http"
 )
 
-var (
-	addr = flag.String("addr", ":8000", "Listen address")
-)
+var addr = ":8002"
 
 func main() {
-	flag.Parse()
-	http.Handle(http_http2.StaticDir, http.StripPrefix(http_http2.StaticDir, http.FileServer(http.Dir("../static"))))
-
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		pusher, ok := w.(http.Pusher)
 		if ok {
-			if err := pusher.Push("/static/bootstrap.min.css", nil); err != nil {
-				log.Printf("Failed to push: %v", err)
-			}
-			if err := pusher.Push("/static/jquery-3.4.1.slim.min.js", nil); err != nil {
-				log.Printf("Failed to push: %v", err)
-			}
-			if err := pusher.Push("/static/popper.min.js", nil); err != nil {
-				log.Printf("Failed to push: %v", err)
-			}
-			if err := pusher.Push("/static/bootstrap.min.js", nil); err != nil {
-				log.Printf("Failed to push: %v", err)
-			}
-			if err := pusher.Push("/static/gopher.png", nil); err != nil {
-				log.Printf("Failed to push: %v", err)
-			}
+			push(pusher, http_http2.Assets, nil)
+
 		}
-		// fmt.Fprintf(w, http_http2.HTML)
-		http_http2.Response(w)
+		fmt.Fprintf(w, http_http2.HTML)
 	})
-	log.Fatal(http.ListenAndServeTLS(*addr, "cert.pem", "key.pem", nil))
+
+	// tlsConfig := &tls.Config{
+	// 	MinVersion:               tls.VersionTLS12,
+	// 	CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+	// 	PreferServerCipherSuites: true,
+	// 	CipherSuites: []uint16{
+	// 		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+	// 		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+	// 		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+	// 		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+	// 	},
+	// }
+
+	// srv := &http.Server{
+	// 	Addr:        addr,
+	// 	WriteTimeout: 15 * time.Second,
+	// 	ReadTimeout:  15 * time.Second,
+	// }
+	//
+	// srv := &http.Server{
+	// 	Addr: addr,
+	// 	// TLSConfig:tlsConfig,
+	// 	// TLSConfig:    tlsConfig,
+	// 	ReadTimeout:       1 * time.Second,
+	// 	WriteTimeout:      1 * time.Second,
+	// 	IdleTimeout:       30 * time.Second,
+	// 	ReadHeaderTimeout: 2 * time.Second,
+	// }
+	// log.Fatal(srv.ListenAndServeTLS("cert.crt", "key.key"))
+	// log.Fatal(srv.ListenAndServeTLS("cert.pem", "key.pem"))
+
+	log.Fatal(http.ListenAndServeTLS(addr, "cert.pem", "key.pem", nil))
+}
+
+func push(pusher http.Pusher, paths []string, options *http.PushOptions) {
+
+	for _, p := range paths {
+		if err := pusher.Push(p, options); err != nil {
+			log.Printf("Failed to push: %v", err)
+		}
+	}
 }
--- https/main.go
+++ https/main.go
@@ -1,22 +1,41 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"github.com/devplayg/http_http2"
 	"log"
 	"net/http"
 )
 
-var (
-	addr = flag.String("addr", ":8000", "Listen address")
-)
+var addr = ":8001"
 
 func main() {
-	flag.Parse()
-	http.Handle(http_http2.StaticDir, http.StripPrefix(http_http2.StaticDir, http.FileServer(http.Dir("../static"))))
-
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http_http2.Response(w)
+		fmt.Fprintf(w, http_http2.HTML)
 	})
-	log.Fatal(http.ListenAndServeTLS(*addr, "cert.pem", "key.pem", nil))
+	log.Fatal(http.ListenAndServeTLS(addr, "cert.pem", "key.pem", nil))
 }
+
+// package main
+//
+// import (
+// 	"flag"
+// 	"github.com/devplayg/http_http2"
+// 	"log"
+// 	"net/http"
+// )
+//
+// var (
+// 	addr = flag.String("addr", ":8000", "Listen address")
+// )
+//
+// func main() {
+// 	flag.Parse()
+// 	http.Handle(http_http2.StaticDir, http.StripPrefix(http_http2.StaticDir, http.FileServer(http.Dir("../static"))))
+//
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		http_http2.Response(w)
+// 	})
+// 	log.Fatal(http.ListenAndServeTLS(*addr, "cert.pem", "key.pem", nil))
+// }
