ad0ae5ee7841e255422565ac0e09c6875c9928b7
devplayg <devplayg@gmail.com>
2020-03-22 19:18:21 +0900

    http

--- http/main.go
+++ http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/devplayg/http_http2"
 	"log"
 	"net/http"
@@ -16,7 +17,7 @@ func main() {
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http_http2.Response(w)
+		fmt.Fprintf(w, http_http2.HTML)
 	})
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
